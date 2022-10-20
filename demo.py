375628eb43c29ac5e8213c0fc0cb83fd2c1e33ef
Arthur Frankel <afrankel@solutionstreet.com>
2022-10-20 13:43:06 -0400

    initial commit

---
+++ demo.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+from flask_cors import CORS
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/demo/', methods=['POST'])
+def respond():
+    curr_num = request.args.get("curr_num", "0")
+
+    new_num = int(curr_num) + 1
+
+    response = f"<button hx-post=\"https://htmxdemo.herokuapp.com/demo/?curr_num={new_num}\" hx-swap=\"outerHTML\">Click Me - {new_num}</button>"
+
+    return response
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
